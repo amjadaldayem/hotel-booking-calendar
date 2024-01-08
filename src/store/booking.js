4082bbc8caabd5916a59add96086a9527767a306
Jad Aldaim <aldaimjad1@gmail.com>
2024-01-08 03:44:04 +0200

    fix: condig store

--- src/store/booking.js
+++ src/store/booking.js
@@ -1,25 +1,50 @@
+import moment from 'moment'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 const state = {
+  selectedDate: moment().format('L'),
+  selectedWeek: [],
   rooms: []
 }
 
 const mutations = {
+  SET_DATE(state, date) {
+    state.selectedDate = date
+  },
+  SET_WEEK(state, week) {
+    state.selectedWeek = week
+  },
   SET_ROOMS(state, rooms) {
     state.rooms = rooms
   }
 }
 
 const actions = {
+  setSelectedDate({ commit }, date) {
+    commit('SET_DATE', date)
+  },
+  setTodayDate({ commit }) {
+    const todayDate = moment().format('L')
+    commit('SET_DATE', todayDate)
+  },
+  setWeek({ commit }, week) {
+    commit('SET_WEEK', week)
+  },
   setHotels({ commit }, rooms) {
     commit('SET_ROOMS', rooms)
   }
 }
 
 const getters = {
+  getSelectedDate(state) {
+    return state.selectedDate
+  },
+  getSelectedWeek(state) {
+    return state.selectedWeek
+  },
   getRooms(state) {
     return state.rooms
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import booking from '@/store/booking'
-import calendar from '@/store/calendar'
 import popup from '@/store/popup'
 import Vuex from 'vuex'
 
 export default new Vuex.Store({
   modules: {
-    calendar,
     booking,
     popup
   }
